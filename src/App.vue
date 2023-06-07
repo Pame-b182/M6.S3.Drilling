125c132b25998d8a417f4e7440419c845da42c52
pamecorreabarrera@gmail.com <pamecorreabarrera@gmail.com>
2023-06-07 14:01:42 -0400

    commit 1: se guardan los cambios realizados al proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container-md">
+        <a class="navbar-brand mx-auto" href="#">Tienda Bit-Game</a>
+      </div>
+    </nav>
+    <div class="alert alert-info m-3" role="alert">
+      <h4 class="alert-heading">¡Bienvenido!</h4>
+      <hr>
+      <p>A continuación encontrarás la lista de los juegos que prefieres.</p>
+    </div>
+    <!-- Aparte de invocar el componente "Games" lo alimento con la props "juegos" que no es un dato literal, 
+    si no que viene del componente, por ello se usa ":" y  "arrayGames" es el dato en el componente padre, que esta entrando a la prop.
+    Se escucha el evento "borrarGame", declarado en el componente hijo, con "@" y
+    se llama al evento "deleteGame" creado en padre, que borrará el juego-->
+    <Games
+    :juegos="arrayGames"
+    @borrarGame="deleteGame"></Games>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Games from '@/components/Games.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      arrayGames: ["Arena of valor", "Pokemon Unite", "Donkey Kong", "Heroes of the storm", "Valorant"]
+    }
+  },
+  methods: {
+    //Se crea el método "deleteGame" que borrará los juegos del array "arrayGames" declarados en "data".
+    //Se recibe el index, llamado ahora "event", enviado por el componente hijo
+    deleteGame(event){
+      //Se aplica el método splice() que elimina el index, 1 a la vez
+      this.arrayGames.splice(event,1)
+    }
+  },
   components: {
-    HelloWorld
+    Games,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Games.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="col col-md-6 mx-auto">
+    <table class="table table-dark text-center">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Juego</th>
+          <th scope="col">Eliminar juego de tu lista</th>
+        </tr>
+      </thead>
+      <!-- Utilizo "v-for" para recorrer el array "arrayGames", me retorna un game y el index,
+      que se encuentran en la prop "juegos". Y utilizo el "index" como la llave -->
+      <tbody v-for="(game, index) in juegos" :key="index">
+        <tr>
+          <th scope="row">{{index}}</th>
+          <td>{{game}}</td>
+          <!-- El método "eliminarGame" recibe el index que provee el "v-for" para encontrar el juego que se quiere eliminar -->
+          <td><button @click="eliminarGame(index)"  type="button" class="btn btn-danger">Eliminar</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "component-game",
+  //Props recibe los datos externos
+  //Para traer los datos que tiene el componente padre, debo utilizar props.
+  //Este dato que llega lo tipeo como array y lo dejo como requerido.
+  props: {
+    juegos:{
+      type: Array,
+      required: true,
+    }
+  },
+  data: function () {
+    return {};
+  },
+  // computed: {},
+  methods: {
+    //Método que elimina el juego al que se le hizo click en el botón "eliminar"
+    //entra el index, para poder ubicar cuál juego
+    eliminarGame(index){
+      //"this.$emit" emite  el evento (para que el padre lo escuche). 
+      //1er argumento: nombre del evento. 2do argumento: le mando al padre el index, para que sepa cuál eliminar
+      this.$emit('borrarGame', index)
+    }
+  }
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
+</script>
+
+<style scoped>
+</style>
